9844d2e21a821e9b83f3a99896c7dae8bacb881f
Naresh <aniljmk@gmail.com>
2020-12-15 19:19:53 +0530

    updates

--- frontend/config/params.php
+++ frontend/config/params.php
@@ -3,5 +3,5 @@ return [
     'building_file_path' =>  \Yii::getAlias('@storage').'/web/buildings/',
     'building_file_path_temp' =>  \Yii::getAlias('@storage').'/web/buildings/temp/',
     'basePath' => dirname(__DIR__),
-    'ContactAuthUserRole'=>['agent','admin'],
+    'ContactAuthUserRole'=>['agent'],
 ];
